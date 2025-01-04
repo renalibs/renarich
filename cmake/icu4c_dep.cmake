a3036dc6f6f78cbc27d8a38a46433a05e76a5e4a
Radom-AutoBot <radomsbot@gmail.com>
2025-01-04 13:39:49 +0000

    sync external files

--- cmake/icu4c_dep.cmake
+++ cmake/icu4c_dep.cmake
@@ -19,11 +19,15 @@ if(WIN32)
     endif()
     string(APPEND ICU4C_PKG_PATH ".zip")
 elseif(UNIX)
-    string(APPEND ICU4C_PKG_URL "UNIX/GCC.tar.gz")
+    string(APPEND ICU4C_PKG_URL "Linux/GCC.tar.gz")
     string(APPEND ICU4C_PKG_PATH ".tar.gz")
     set(ICU4C_PKG_HASH "3d1d5e68fb58a462df1d80c7ce4c836b8dfb14e63851cad76cdfe88b914d6208")
+elseif(APPLE)
+    string(APPEND ICU4C_PKG_URL "MacOS/Clang.tar.gz")
+    string(APPEND ICU4C_PKG_PATH ".tar.gz")
+    set(ICU4C_PKG_HASH "06c7dfd0f8065d68c52efbfc876e166c4ca295ca06fb3d47109aa0b08448a025")
 else()
-    message(FATAL_ERROR "Other platforms except Windows and Unix-like are not supported.")
+    message(FATAL_ERROR "Other platforms except Windows, Unix-like or MacOS are not supported.")
 endif()
 
 if(EXISTS ${ICU4C_BIN_DIR})
@@ -50,36 +54,42 @@ message(STATUS "Extracting ICU4C binary package...")
 file(MAKE_DIRECTORY ${ICU4C_BIN_DIR})
 if(WIN32)
     execute_process(
-        COMMAND powershell -Command \"Expand-Archive -Path ${ICU4C_PKG_PATH} -DestinationPath ${ICU4C_BIN_DIR}\"
+        COMMAND powershell -Command Expand-Archive -Path ${ICU4C_PKG_PATH} -DestinationPath ${ICU4C_BIN_DIR}
         COMMAND_ERROR_IS_FATAL ANY
     )
     if(MSVC)
         set(ICU4C_ROOT_DIR "${ICU4C_BIN_DIR}/MSVC2022" CACHE STRING INTERNAL)
         set(ICU4C_LIB_BIN_DIR "${ICU4C_ROOT_DIR}/bin64")
         set(ICU4C_LIB_LIB_DIR "${ICU4C_ROOT_DIR}/lib64")
+        set(ICU4C_INCLUDE_DIR "${ICU4C_ROOT_DIR}/include" CACHE STRING INTERNAL)
+        set(ICU4C_LIB_DATA "${ICU4C_LIB_LIB_DIR}/icudt.lib" CACHE STRING INTERNAL)
+        set(ICU4C_LIB_COMMON "${ICU4C_LIB_LIB_DIR}/icuuc.lib" CACHE STRING INTERNAL)
+        set(ICU4C_LIB_I18N "${ICU4C_LIB_LIB_DIR}/icuin.lib" CACHE STRING INTERNAL)
+        set(ICU4C_LIB_IO "${ICU4C_LIB_LIB_DIR}/icuio.lib" CACHE STRING INTERNAL)
+        set(ICU4C_LIB_TOOL_UTILS "${ICU4C_LIB_LIB_DIR}/icutu.lib" CACHE STRING INTERNAL)
     else()
         set(ICU4C_ROOT_DIR "${ICU4C_BIN_DIR}/MinGW" CACHE STRING INTERNAL)
         set(ICU4C_LIB_BIN_DIR "${ICU4C_ROOT_DIR}/bin")
         set(ICU4C_LIB_LIB_DIR "${ICU4C_ROOT_DIR}/lib")
+        set(ICU4C_INCLUDE_DIR "${ICU4C_ROOT_DIR}/include" CACHE STRING INTERNAL)
+        set(ICU4C_LIB_DATA "${ICU4C_LIB_BIN_DIR}/icudt76.dll" CACHE STRING INTERNAL)
+        set(ICU4C_LIB_COMMON "${ICU4C_LIB_BIN_DIR}/icuuc76.dll" CACHE STRING INTERNAL)
+        set(ICU4C_LIB_I18N "${ICU4C_LIB_BIN_DIR}/icuin76.dll" CACHE STRING INTERNAL)
+        set(ICU4C_LIB_IO "${ICU4C_LIB_BIN_DIR}/icuio76.dll" CACHE STRING INTERNAL)
+        set(ICU4C_LIB_TOOL_UTILS "${ICU4C_LIB_BIN_DIR}/icutu76.dll" CACHE STRING INTERNAL)
     endif()
-    set(ICU4C_INCLUDE_DIR "${ICU4C_ROOT_DIR}/include" CACHE STRING INTERNAL)
-    set(ICU4C_LIB_DATA "${ICU4C_LIB_BIN_DIR}/icudt76.dll" CACHE STRING INTERNAL)
     set(ICU4C_LIB_DATA_FILE_LIST
         "${ICU4C_LIB_BIN_DIR}/icudt76.dll"
     CACHE STRING INTERNAL)
-    set(ICU4C_LIB_COMMON "${ICU4C_LIB_BIN_DIR}/icuuc76.dll" CACHE STRING INTERNAL)
     set(ICU4C_LIB_COMMON_FILE_LIST
         "${ICU4C_LIB_BIN_DIR}/icuuc76.dll"
     CACHE STRING INTERNAL)
-    set(ICU4C_LIB_I18N "${ICU4C_LIB_BIN_DIR}/icuin76.dll" CACHE STRING INTERNAL)
     set(ICU4C_LIB_I18N_FILE_LIST
         "${ICU4C_LIB_BIN_DIR}/icuin76.dll"
     CACHE STRING INTERNAL)
-    set(ICU4C_LIB_IO "${ICU4C_LIB_BIN_DIR}/icuio76.dll" CACHE STRING INTERNAL)
     set(ICU4C_LIB_IO_FILE_LIST
         "${ICU4C_LIB_BIN_DIR}/icuio76.dll"
     CACHE STRING INTERNAL)
-    set(ICU4C_LIB_TOOL_UTILS "${ICU4C_LIB_BIN_DIR}/icutu76.dll" CACHE STRING INTERNAL)
     set(ICU4C_LIB_TOOL_UTILS_FILE_LIST
         "${ICU4C_LIB_BIN_DIR}/icutu76.dll"
     CACHE STRING INTERNAL)
@@ -88,37 +98,43 @@ else()
         COMMAND tar -zxf ${ICU4C_PKG_PATH} -C ${ICU4C_BIN_DIR}
         COMMAND_ERROR_IS_FATAL ANY
     )
-    set(ICU4C_ROOT_DIR "${ICU4C_BIN_DIR}/GCC" CACHE STRING INTERNAL)
+    if(UNIX)
+        set(ICU4C_ROOT_DIR "${ICU4C_BIN_DIR}/GCC" CACHE STRING INTERNAL)
+        set(PLATFORM_DLL_EXT "so")
+    else()
+        set(ICU4C_ROOT_DIR "${ICU4C_BIN_DIR}/Clang" CACHE STRING INTERNAL)
+        set(PLATFORM_DLL_EXT "dylib")
+    endif()
     set(ICU4C_INCLUDE_DIR "${ICU4C_ROOT_DIR}/include" CACHE STRING INTERNAL)
-    set(ICU4C_LIB_DATA "${ICU4C_ROOT_DIR}/lib/libicudata.so" CACHE STRING INTERNAL)
+    set(ICU4C_LIB_DATA "${ICU4C_ROOT_DIR}/lib/libicudata.${PLATFORM_DLL_EXT}" CACHE STRING INTERNAL)
     set(ICU4C_LIB_DATA_FILE_LIST
-        "${ICU4C_ROOT_DIR}/lib/libicudata.so"
-        "${ICU4C_ROOT_DIR}/lib/libicudata.so.76"
-        "${ICU4C_ROOT_DIR}/lib/libicudata.so.76.1"
+        "${ICU4C_ROOT_DIR}/lib/libicudata.${PLATFORM_DLL_EXT}"
+        "${ICU4C_ROOT_DIR}/lib/libicudata.${PLATFORM_DLL_EXT}.76"
+        "${ICU4C_ROOT_DIR}/lib/libicudata.${PLATFORM_DLL_EXT}.76.1"
     CACHE STRING INTERNAL)
-    set(ICU4C_LIB_COMMON "${ICU4C_ROOT_DIR}/lib/libicuuc.so" CACHE STRING INTERNAL)
+    set(ICU4C_LIB_COMMON "${ICU4C_ROOT_DIR}/lib/libicuuc.${PLATFORM_DLL_EXT}" CACHE STRING INTERNAL)
     set(ICU4C_LIB_COMMON_FILE_LIST
-        "${ICU4C_ROOT_DIR}/lib/libicuuc.so"
-        "${ICU4C_ROOT_DIR}/lib/libicuuc.so.76"
-        "${ICU4C_ROOT_DIR}/lib/libicuuc.so.76.1"
+        "${ICU4C_ROOT_DIR}/lib/libicuuc.${PLATFORM_DLL_EXT}"
+        "${ICU4C_ROOT_DIR}/lib/libicuuc.${PLATFORM_DLL_EXT}.76"
+        "${ICU4C_ROOT_DIR}/lib/libicuuc.${PLATFORM_DLL_EXT}.76.1"
     CACHE STRING INTERNAL)
-    set(ICU4C_LIB_I18N "${ICU4C_ROOT_DIR}/lib/libicui18n.so" CACHE STRING INTERNAL)
+    set(ICU4C_LIB_I18N "${ICU4C_ROOT_DIR}/lib/libicui18n.${PLATFORM_DLL_EXT}" CACHE STRING INTERNAL)
     set(ICU4C_LIB_I18N_FILE_LIST
-        "${ICU4C_ROOT_DIR}/lib/libicui18n.so"
-        "${ICU4C_ROOT_DIR}/lib/libicui18n.so.76"
-        "${ICU4C_ROOT_DIR}/lib/libicui18n.so.76.1"
+        "${ICU4C_ROOT_DIR}/lib/libicui18n.${PLATFORM_DLL_EXT}"
+        "${ICU4C_ROOT_DIR}/lib/libicui18n.${PLATFORM_DLL_EXT}.76"
+        "${ICU4C_ROOT_DIR}/lib/libicui18n.${PLATFORM_DLL_EXT}.76.1"
     CACHE STRING INTERNAL)
-    set(ICU4C_LIB_IO "${ICU4C_ROOT_DIR}/lib/libicuio.so" CACHE STRING INTERNAL)
+    set(ICU4C_LIB_IO "${ICU4C_ROOT_DIR}/lib/libicuio.${PLATFORM_DLL_EXT}" CACHE STRING INTERNAL)
     set(ICU4C_LIB_IO_FILE_LIST
-        "${ICU4C_ROOT_DIR}/lib/libicuio.so"
-        "${ICU4C_ROOT_DIR}/lib/libicuio.so.76"
-        "${ICU4C_ROOT_DIR}/lib/libicuio.so.76.1"
+        "${ICU4C_ROOT_DIR}/lib/libicuio.${PLATFORM_DLL_EXT}"
+        "${ICU4C_ROOT_DIR}/lib/libicuio.${PLATFORM_DLL_EXT}.76"
+        "${ICU4C_ROOT_DIR}/lib/libicuio.${PLATFORM_DLL_EXT}.76.1"
     CACHE STRING INTERNAL)
-    set(ICU4C_LIB_TOOL_UTILS "${ICU4C_ROOT_DIR}/lib/libicuuc.so" CACHE STRING INTERNAL)
+    set(ICU4C_LIB_TOOL_UTILS "${ICU4C_ROOT_DIR}/lib/libicuuc.${PLATFORM_DLL_EXT}" CACHE STRING INTERNAL)
     set(ICU4C_LIB_TOOL_UTILS_FILE_LIST
-        "${ICU4C_ROOT_DIR}/lib/libicuuc.so"
-        "${ICU4C_ROOT_DIR}/lib/libicuuc.so.76"
-        "${ICU4C_ROOT_DIR}/lib/libicuuc.so.76.1"
+        "${ICU4C_ROOT_DIR}/lib/libicuuc.${PLATFORM_DLL_EXT}"
+        "${ICU4C_ROOT_DIR}/lib/libicuuc.${PLATFORM_DLL_EXT}.76"
+        "${ICU4C_ROOT_DIR}/lib/libicuuc.${PLATFORM_DLL_EXT}.76.1"
     CACHE STRING INTERNAL)
 endif()
 message(STATUS "Extract completed.")
